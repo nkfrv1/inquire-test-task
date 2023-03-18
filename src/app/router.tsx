50d20638717330dcc5e513b368be098390497420
nkfrv1 <i.nikiforov1111@gmail.com>
2023-03-18 20:20:37 +0200

    made posts page the index page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,14 +1,13 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import { createBrowserRouter, redirect, RouteObject } from 'react-router-dom';
 import Post from '../components/Post';
 import ErrorPage from '../pages/Error';
-import HomePage from '../pages/Home';
 import PostsPage from '../pages/Posts';
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <HomePage />,
     errorElement: <ErrorPage />,
+    loader: () => redirect('/posts'),
   },
   {
     path: '/posts',
